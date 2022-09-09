aca11c7ee5acd5cf37a0c290ac760f0230b1bfa5
absitnikov <ab.sitnikov@mail.ru>
2022-09-09 12:28:01 +0500

    fixed 4 issue

--- select.sql
+++ select.sql
@@ -11,11 +11,12 @@ SELECT album_name, AVG(track_duration) AS averagetrack FROM album a
 JOIN track t ON a.album_id = t.album_id 
 GROUP BY album_name;
 
-SELECT musician_name FROM musician a 
-JOIN musician_album b ON a.musician_id = b.musician_id 
-JOIN album c ON b.album_id = c.album_id 
-WHERE EXTRACT (YEAR FROM album_year) != 2020
-GROUP BY musician_name;
+
+SELECT DISTINCT musician_name FROM musician m
+WHERE musician_name  NOT IN (SELECT DISTINCT musician_name FROM musician m  
+LEFT JOIN musician_album ma ON m.musician_id = ma.musician_id 
+LEFT JOIN album a ON ma.album_id = a.album_id
+WHERE EXTRACT (YEAR FROM album_year) = 2020);
 
 SELECT collection_name, musician_name FROM collection c 
 JOIN track_collection tk USING(collection_id)
@@ -23,8 +24,7 @@ JOIN track t USING(track_id)
 JOIN album a USING(album_id)
 JOIN musician_album ma USING(album_id)
 JOIN musician m USING(musician_id)
-WHERE musician_name LIKE 'Pink Floyd'
-GROUP BY collection_name, musician_name;
+WHERE musician_name LIKE 'Pink Floyd';
 
 SELECT album_name, musician_name, COUNT(genre_name) AS genre_count FROM album a 
 JOIN musician_album ma USING(album_id)
@@ -45,12 +45,13 @@ JOIN album a USING(album_id)
 JOIN track t USING(album_id)
 WHERE track_duration = (SELECT MIN(track_duration) FROM track);
 
-SELECT album_name, COUNT(track_name) AS track_count FROM album a 
+SELECT album_name, COUNT(track_name) AS track_duration FROM album a 
 JOIN track t USING(album_id)
 GROUP BY album_name
 HAVING COUNT(track_name) = (
-	SELECT COUNT(track_name) AS count_track FROM track tc
+	SELECT COUNT(track_name) AS track_duration FROM track tc
 	JOIN album ac USING(album_id)
 	GROUP BY album_name
-	ORDER BY count_track
+	ORDER BY track_duration
 	LIMIT 1);
+
